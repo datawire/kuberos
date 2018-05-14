1a66d2f99f8f46a2f83efa629c30df941350cd0a
Philip Lombardi <plombardi@datawire.io>
2018-05-14 16:04:34 -0400

    travis encrypt defucking part 4

--- ci/extract_secrets_archive.sh
+++ ci/extract_secrets_archive.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -exo pipefail
 
-openssl aes-256-cbc -K $encrypted_37d2faef682c_key -iv $encrypted_37d2faef682c_iv -in ci/secrets.tar.enc -out ci/secrets.tar -d
-
 tar xvf ci/secrets.tar
 
 mkdir -p ~/.aws
